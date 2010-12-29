2b9ff472d3a81e4b9d62140cb89bc9a235207602
Dave Doolin <david.doolin@gmail.com>
2010-12-29 13:33:12 -0800

    Added paths, will rename

--- worldpress_helloworld.php
+++ worldpress_helloworld.php
@@ -4,4 +4,30 @@
  *
  */
 
+if (!class_exists("demo_plugin_options")) {
+	
+    class demo_plugin_options {
+
+        function demo_plugin_options() {
+            add_action('admin_menu', array(&$this, 'on_admin_menu'));
+        }
+
+        function on_admin_menu() {
+            add_options_page('Demo Paths Page', 'Demo Paths', 8, __FILE__ , array (&$this, 'paths_options'));
+        }
+
+        function paths_options() {
+        	/**
+        	 * Move all the html to it's own
+        	 * file, makes everything much cleaner.
+        	 */
+            include('paths.html');	
+		}
+        
+    }
+	
+}
+
+$wpdpd = new demo_plugin_paths();
+
 ?>
